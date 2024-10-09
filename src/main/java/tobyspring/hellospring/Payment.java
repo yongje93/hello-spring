47343c328b2e7df36ab8876f9cdceeccb7a5b2ea
Yongje Shin <yongje1211@gmail.com>
2024-10-09 23:27:21 +0900

    feat: PaymentService 개발

---
+++ src/main/java/tobyspring/hellospring/Payment.java
@@ -0,0 +1,28 @@
+package tobyspring.hellospring;
+
+import lombok.Getter;
+import lombok.ToString;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Getter
+@ToString
+public class Payment {
+
+    private Long orderId;
+    private String currency;
+    private BigDecimal foreignCurrencyAmount;
+    private BigDecimal exRate;
+    private BigDecimal convertedAmount;
+    private LocalDateTime validUntil;
+
+    public Payment(Long orderId, String currency, BigDecimal foreignCurrencyAmount, BigDecimal exRate, BigDecimal convertedAmount, LocalDateTime validUntil) {
+        this.orderId = orderId;
+        this.currency = currency;
+        this.foreignCurrencyAmount = foreignCurrencyAmount;
+        this.exRate = exRate;
+        this.convertedAmount = convertedAmount;
+        this.validUntil = validUntil;
+    }
+}

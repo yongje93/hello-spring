2390dd5d493e0418b319f17ca46e3032fe240276
Yongje Shin <yongje1211@gmail.com>
2024-12-17 10:59:52 +0900

    feat: ApiExecutor 콜백과 메소드 주입

--- src/main/java/tobyspring/hellospring/exrate/WebApiExRateProvider.java
+++ src/main/java/tobyspring/hellospring/exrate/WebApiExRateProvider.java
@@ -6,6 +6,7 @@ import java.io.IOException;
 import java.math.BigDecimal;
 import java.net.URI;
 import java.net.URISyntaxException;
+import tobyspring.hellospring.api.ApiExecutor;
 import tobyspring.hellospring.api.SimpleApiExecutor;
 import tobyspring.hellospring.payment.ExRateProvider;
 
@@ -15,10 +16,10 @@ public class WebApiExRateProvider implements ExRateProvider {
     public BigDecimal getExRate(String currency) {
         String url = "https://open.er-api.com/v6/latest/" + currency;
 
-        return runApiForExRate(url);
+        return runApiForExRate(url, new SimpleApiExecutor());
     }
 
-    private BigDecimal runApiForExRate(String url) {
+    private BigDecimal runApiForExRate(String url, ApiExecutor apiExecutor) {
         URI uri;
         try {
             uri = new URI(url);
@@ -28,7 +29,7 @@ public class WebApiExRateProvider implements ExRateProvider {
 
         String response;
         try {
-            response = new SimpleApiExecutor().execute(uri);
+            response = apiExecutor.execute(uri);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }

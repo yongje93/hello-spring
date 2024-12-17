4dee060490f3fd2fc0d42b1ac3e984e8d5a62913
Yongje Shin <yongje1211@gmail.com>
2024-12-17 17:01:51 +0900

    refactor: lombok 제거

--- src/main/java/tobyspring/hellospring/payment/Payment.java
+++ src/main/java/tobyspring/hellospring/payment/Payment.java
@@ -1,14 +1,9 @@
 package tobyspring.hellospring.payment;
 
-import lombok.Getter;
-import lombok.ToString;
-
 import java.math.BigDecimal;
 import java.time.Clock;
 import java.time.LocalDateTime;
 
-@Getter
-@ToString
 public class Payment {
 
     private Long orderId;
@@ -18,7 +13,8 @@ public class Payment {
     private BigDecimal convertedAmount;
     private LocalDateTime validUntil;
 
-    public Payment(Long orderId, String currency, BigDecimal foreignCurrencyAmount, BigDecimal exRate, BigDecimal convertedAmount, LocalDateTime validUntil) {
+    public Payment(Long orderId, String currency, BigDecimal foreignCurrencyAmount, BigDecimal exRate, BigDecimal convertedAmount,
+        LocalDateTime validUntil) {
         this.orderId = orderId;
         this.currency = currency;
         this.foreignCurrencyAmount = foreignCurrencyAmount;
@@ -27,7 +23,8 @@ public class Payment {
         this.validUntil = validUntil;
     }
 
-    public static Payment createPrepared(Long orderId, String currency, BigDecimal foreignCurrencyAmount, BigDecimal exRate, LocalDateTime now) {
+    public static Payment createPrepared(Long orderId, String currency, BigDecimal foreignCurrencyAmount, BigDecimal exRate,
+        LocalDateTime now) {
         BigDecimal convertedAmount = foreignCurrencyAmount.multiply(exRate);
         LocalDateTime validUntil = now.plusMinutes(30);
         return new Payment(orderId, currency, foreignCurrencyAmount, exRate, convertedAmount, validUntil);
@@ -35,5 +32,41 @@ public class Payment {
 
     public boolean isValid(Clock clock) {
         return LocalDateTime.now(clock).isBefore(this.validUntil);
+    }
+
+    public Long getOrderId() {
+        return orderId;
+    }
+
+    public String getCurrency() {
+        return currency;
+    }
+
+    public BigDecimal getForeignCurrencyAmount() {
+        return foreignCurrencyAmount;
+    }
+
+    public BigDecimal getExRate() {
+        return exRate;
+    }
+
+    public BigDecimal getConvertedAmount() {
+        return convertedAmount;
+    }
+
+    public LocalDateTime getValidUntil() {
+        return validUntil;
+    }
+
+    @Override
+    public String toString() {
+        return "Payment{" +
+            "orderId=" + orderId +
+            ", currency='" + currency + '\'' +
+            ", foreignCurrencyAmount=" + foreignCurrencyAmount +
+            ", exRate=" + exRate +
+            ", convertedAmount=" + convertedAmount +
+            ", validUntil=" + validUntil +
+            '}';
     }
 }

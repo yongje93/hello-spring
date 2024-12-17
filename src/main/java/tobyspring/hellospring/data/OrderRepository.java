96e52706053db7c02443f77819c0385c4b9f9196
Yongje Shin <yongje1211@gmail.com>
2024-12-17 17:02:16 +0900

    feat: JPA를 이용한 Order 저장

---
+++ src/main/java/tobyspring/hellospring/data/OrderRepository.java
@@ -0,0 +1,15 @@
+package tobyspring.hellospring.data;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import tobyspring.hellospring.order.Order;
+
+public class OrderRepository {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    public void save(Order order) {
+        entityManager.persist(order);
+    }
+}

097f2652005f4b3163fb503f8cce1e429cf66321
Yongje Shin <yongje1211@gmail.com>
2024-12-18 10:13:28 +0900

    test: OrderServiceSpringTest

---
+++ src/test/java/tobyspring/hellospring/order/OrderServiceSpringTest.java
@@ -0,0 +1,26 @@
+package tobyspring.hellospring.order;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.math.BigDecimal;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import tobyspring.hellospring.OrderConfig;
+
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = OrderConfig.class)
+public class OrderServiceSpringTest {
+
+    @Autowired
+    OrderService orderService;
+
+    @Test
+    void createOrder() {
+        var order = orderService.createOrder("01000", BigDecimal.ONE);
+
+        assertThat(order.getId()).isGreaterThan(0);
+    }
+}
